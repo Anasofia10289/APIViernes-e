fc681a5efac14af0a5540ad4089c11f30831dd94
Paco_Programador <143565623+CristianProgra@users.noreply.github.com>
2025-06-13 08:24:36 -0500

    se crea api examen

---
+++ src/main/java/com/example/APIViernes/ApiViernesApplication.java
@@ -0,0 +1,13 @@
+package com.example.APIViernes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiViernesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiViernesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/APIViernes/ApiViernesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.APIViernes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiViernesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
